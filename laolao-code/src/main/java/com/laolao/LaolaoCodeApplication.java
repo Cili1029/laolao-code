95ef18ce6ee07621b1179219adda8d175cb52567
Cili <3317309371@qq.com>
2026-01-31 19:21:14 +0800

    配置项目

---
+++ laolao-code/src/main/java/com/laolao/LaolaoCodeApplication.java
@@ -0,0 +1,12 @@
+package com.laolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaolaoCodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LaolaoCodeApplication.class, args);
+    }
+
+}
---
+++ laolao-code/src/test/java/com/laolao/LaolaoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaolaoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
